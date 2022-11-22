05da3758528cb6cc2d9f98d075fdbf51e703f9ff
Danila <74421196+dendd1@users.noreply.github.com>
2022-11-22 10:27:38 +0300

    left part

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,168 @@
 <template>
   <div class="m-5">
-    <div class="border box p-2 mb-3">
-      <h1 class="fs-3 m-4">Футы в метры</h1>
-      <div class="row ms-4">
-        <div class="col">
-          <div class="d-flex">
-            <p class="fs-4 me-2">Футы:</p>
-            <input v-model="feet" @input="convertToMeter" class="input-site border fs-4 p-2 w-100" type="text">
+
+
+
+    <div class="row">
+      <div class="col">
+        <h1 class="fs-3 m-3">Заполните форму:</h1>
+        <form>
+
+
+          <div class="row">
+            <div class="col-3">
+              <p class="fs-5">Профессия</p>
+            </div>
+            <div class="col-9">
+              <input v-model="profession" class="input-site border fs-5 p-2" type="text">
+            </div>
           </div>
 
-        </div>
-        <div class="col">
-          <div class="d-flex">
-            <p class="fs-4 me-2">Метр:</p>
-            <input v-model="meter" @input="convertToFeet" class="input-site border fs-4 p-2 w-100" type="text">
+
+          <div class="row">
+            <div class="col-3">
+              <p class="fs-5">Город</p>
+            </div>
+            <div class="col-9">
+              <input v-model="city" class="input-site border fs-5 p-2 " type="text">
+            </div>
           </div>
 
+
+
+          <div class="row">
+            <div class="col-3">
+              <p class="fs-5">Фото</p>
+            </div>
+            <div class="col-9">
+              <input v-model="photo" class="input-site border fs-5 p-2 " type="text">
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-3">
+              <p class="fs-5">Фалимия</p>
+            </div>
+            <div class="col-9">
+              <input v-model="surname" class="input-site border fs-5 p-2 " type="text">
+            </div>
+          </div>
+
+
+          <div class="row">
+            <div class="col-3">
+              <p class="fs-5">Имя</p>
+            </div>
+            <div class="col-9">
+              <input v-model="name" class="input-site border fs-5 p-2 " type="text">
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-3">
+              <p class="fs-5">Отчесво</p>
+            </div>
+            <div class="col-9">
+              <input v-model="patronymic" class="input-site border fs-5 p-2 " type="text">
+            </div>
+          </div>
+
+
+          <div class="row">
+            <div class="col-3">
+              <p class="fs-5">Телефон</p>
+            </div>
+            <div class="col-9">
+              <input v-model="phone" class="input-site border fs-5 p-2 " type="text">
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-3">
+              <p class="fs-5">Почта</p>
+            </div>
+            <div class="col-9">
+              <input v-model="mail" class="input-site border fs-5 p-2 " type="text">
+            </div>
+          </div>
+
+
+          <div class="row">
+            <div class="col-3">
+              <p class="fs-5">Дата рождения</p>
+            </div>
+            <div class="col-9">
+              <input v-model="bDate" class="input-site border fs-5 p-2 " type="date">
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-3">
+              <p class="fs-5">Образование</p>
+            </div>
+            <div class="col-9">
+              <input v-model="education" class="input-site border fs-5 p-2 " type="text">
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-3">
+              <p class="fs-5">Желаемая ЗП</p>
+            </div>
+            <div class="col-9">
+              <input v-model="money" class="input-site border fs-5 p-2 " type="text">
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-3">
+              <p class="fs-5">Ключевые навыки</p>
+            </div>
+            <div class="col-9">
+              <input v-model="skills" class="input-site border fs-5 p-2 " type="text">
+            </div>
+          </div>
+
+
+          <div class="row">
+            <div class="col-3">
+              <p class="fs-5">О себе</p>
+            </div>
+            <div class="col-9">
+              <textarea  v-model="about" class="input-site border fs-5 p-2" style="height: 200px;" rows="7"></textarea>
+            </div>
+          </div>
+
+        </form>
+      </div>
+      <div class="col">
+        <h1>Ваше резюме</h1>
+        <div class="border p-3">
+          <p class="fs-5">Профессия</p>
+          <p class="fs-5">{{ profession }}</p>
+          <p class="fs-5">Город</p>
+          <p class="fs-5">{{ city }}</p>
+          <p class="fs-5">Фото</p>
+          <p class="fs-5">{{ photo }}</p>
+          <p class="fs-5">ФИО</p>
+          <p class="fs-5">{{ surname }} {{name}} {{patronymic}}</p>
+          <p class="fs-5">Телефон</p>
+          <p class="fs-5">{{ phone }}</p>
+          <p class="fs-5">Почта</p>
+          <p class="fs-5">{{ mail }}</p>
+          <p class="fs-5">Дата рождения</p>
+          <p class="fs-5">{{ bDate }}</p>
+          <p class="fs-5">Образование</p>
+          <p class="fs-5">{{ education }}</p>
+          <p class="fs-5">Желаемая ЗП</p>
+          <p class="fs-5">{{ money }}</p>
+          <p class="fs-5">Навыки</p>
+          <p class="fs-5">{{ skills }}</p>
+          <p class="fs-5">О себе</p>
+          <p class="fs-5">{{ about }}</p>
         </div>
       </div>
     </div>
-
-
-
-    <div class="border box p-2 mb-3">
-      <h1 class="fs-3 m-4">Мили в километры</h1>
-      <div class="row ms-4">
-        <div class="col">
-          <div class="d-flex">
-            <p class="fs-4 me-2">Мили:</p>
-            <input v-model="mile" @input="convertToKilometer" class="input-site border fs-4 p-2 w-100" type="text">
-          </div>
-
-        </div>
-        <div class="col">
-          <div class="d-flex">
-            <p class="fs-4 me-2">Километр:</p>
-            <input v-model="kilometer" @input="convertToMile" class="input-site border fs-4 p-2 w-100" type="text">
-          </div>
-
-        </div>
-      </div>
-    </div>
-
-
-    <div class="border box p-2 mb-3">
-      <h1 class="fs-3 m-4">Градусы в радианы</h1>
-      <div class="row ms-4">
-        <div class="col">
-          <div class="d-flex">
-            <p class="fs-4 me-2">Градусы:</p>
-            <input v-model="degree" @input="convertToRadian" class="input-site border fs-4 p-2 w-100" type="text">
-          </div>
-
-        </div>
-        <div class="col">
-          <div class="d-flex">
-            <p class="fs-4 me-2">Радианы:</p>
-            <input v-model="radian" @input="convertToDegree" class="input-site border fs-4 p-2 w-100" type="text">
-          </div>
-
-        </div>
-      </div>
-    </div>
-
-    <div class="border box p-2 mb-3">
-      <h1 class="fs-3 m-4">C в F</h1>
-      <div class="row ms-4">
-        <div class="col">
-          <div class="d-flex">
-            <p class="fs-4 me-2">C:</p>
-            <input v-model="celsius" @input="convertToFahrenheit" class="input-site border fs-4 p-2 w-100" type="text">
-          </div>
-
-        </div>
-        <div class="col">
-          <div class="d-flex">
-            <p class="fs-4 me-2">F:</p>
-            <input v-model="fahrenheit" @input="convertToCelsius" class="input-site border fs-4 p-2 w-100" type="text">
-          </div>
-
-        </div>
-      </div>
-    </div>
-
   </div>
 </template>
 
@@ -93,15 +173,19 @@ export default {
       feet: 0,
       meter: 0,
       fToM: 3.28,
-      mile: 0,
-      kilometer: 0,
-      mToK: 1.85,
-      degree: 0,
-      radian: 0,
-      rToD: 57.3,
-      celsius: 0,
-      fahrenheit: 0,
-      fToC: 33.8,
+      profession: '',
+      city: '',
+      photo: '',
+      surname: '',
+      name: '',
+      patronymic: '',
+      phone: '',
+      mail: '',
+      bDate: '',
+      education: '',
+      money: '',
+      skills: '',
+      about: '',
     }
   },
   methods: {
@@ -111,24 +195,7 @@ export default {
     convertToFeet() {
       this.feet = this.meter * this.fToM;
     },
-    convertToMile() {
-      this.mile = this.kilometer / this.mToK;
-    },
-    convertToKilometer() {
-      this.kilometer = this.mile * this.mToK;
-    },
-    convertToDegree() {
-      this.degree = this.radian * this.rToD;
-    },
-    convertToRadian() {
-      this.radian = this.degree / this.rToD;
-    },
-    convertToCelsius() {
-      this.celsius = this.fahrenheit / this.fToC;
-    },
-    convertToFahrenheit() {
-      this.fahrenheit = this.celsius * this.fToC;
-    },
+
   }
 }
 </script>
@@ -141,5 +208,6 @@ export default {
 .input-site {
   height: 35px;
   border-radius: 3px;
+  width: 400px;
 }
 </style>
